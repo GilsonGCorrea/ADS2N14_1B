57fad734d4e1f566a2141419743e560e04a143ee
GilsonGCorrea <gilson@ALFANB19.alfasys.local>
2014-02-27 16:21:57 -0300

    Added Hello

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
@@ -7,8 +8,21 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// esta linha de comando exibe a mensagem que é digitada nela entre aspas
-		System.out.println(" Hello World !");
+		// criada variável ler  recebe o que for digitado no teclado;
+		Scanner ler = new Scanner(System.in);
+		
+		//variável username, criada para receber o que for captado pela variável ler;
+		String username;
+		
+		//exibe mensagem na tela solicitando que o susuário digite o nome;
+		System.out.println("Digite seu nome:");
+		
+		//username recebe o que for digitado e captado pela varíavel ler;
+		username = ler.next();
+		
+		// Exibe mensagem na tela;
+		System.out.println("Hello "+username+"!!!");
+		
 	}
 
 }
